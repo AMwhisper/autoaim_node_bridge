947d7e6ba9301d83950df1842f42003c82d3ec25
AMwhisper <3056422950@qq.com>
2024-07-15 17:42:32 +0800

    Second Commit

--- app/app.py
+++ app/app.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+'/../node_bridge/node_bridge/')
+# sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+'/src/node_bridge_ros2')
+
+# 定义要引用的ROS2包列表
+ros2_packages = ['autoaim', 'node_bridge_ros2', 'node_interface']
+workspace_dir = os.path.expanduser('~/ros/workspace/nbr2_ws')
+# 将每个ROS2包的路径添加到sys.path
+for package_name in ros2_packages:
+    packages_path = os.path.join(workspace_dir,'src', package_name)
+    sys.path.insert(0, packages_path)
+
 from node_bridge_ros2 import protocol 
 from node_bridge_ros2 import bridge as nbbridge
 import autoaim
---
+++ app/test.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 定义要引用的ROS2包列表
+ros2_packages = ['autoaim', 'node_bridge_ros2', 'node_interface']
+
+workspace_dir = os.path.expanduser('~/ros/workspace/nbr2_ws')
+for package_name in ros2_packages:
+    ros2_package_path = os.path.join(current_dir, '..', '..', 'src', package_name, package_name)
+    packages_path = os.path.join(workspace_dir,'src', package_name)
+    print (ros2_package_path)
+
+    print (packages_path)
+    sys.path.insert(0, ros2_package_path)
+
+
